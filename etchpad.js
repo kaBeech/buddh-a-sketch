8174bdf897eac4b69f88ed7a199e449ef3ae4740
QSL <kbeechly@icloud.com>
2022-07-26 23:16:22 -0700

    Fix JS syntax

--- etchpad.js
+++ etchpad.js
@@ -3,8 +3,8 @@ const body = document.querySelector('body');
 function addBox16(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
-    div.classList.add = 'box16';
-    div.classList.add = 'flex';
+    div.classList.add('box16');
+    div.classList.add('flex');
 }
 
 addBox16(body);
@@ -13,33 +13,34 @@ addBox16(body);
 function addColumnContainer(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
-    div.classList.add = 'columnContainer';
-    div.classList.add = 'flex';
-    div.classList.add = 'column';
+    div.classList.add('columnContainer');
+    div.classList.add('flex');
+    div.classList.add('column');
 }
 
 function addCellDiv(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
-    div.classList.add = 'cell';
+    div.classList.add('cell');
 }
 
 function populateColumnContainer() {
     const columns = document.querySelectorAll('.columnContainer');
-    const column = columns[-1];
-    for (i = 0; i>16; i++) {
-        addCellDiv(column)
+    const column = columns[columns.length - 1];
+    for (i = 0; i<16; i++) {
+        addCellDiv(column);
     }
 }
 
 function populateBox16() {
     const box16 = document.querySelector('.box16');
-    for (i = 0; i>16; i++) {
+    for (i = 0; i<16; i++) {
         addColumnContainer(box16);
         populateColumnContainer();
     }
 }
 
+// populateBox16();
 
 // addDiv(body);
 
