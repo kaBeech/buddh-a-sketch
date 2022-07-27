4191e61fc31b56a77a68b4c78fc16e9b411565a1
QSL <kbeechly@icloud.com>
2022-07-26 21:28:50 -0700

    Add pseudocode for 16x16 grid, add addDiv function

--- etchpad.js
+++ etchpad.js
@@ -1,1 +1,19 @@
 const body = document.querySelector('body');
+
+function addDiv(parentNode) {
+    const div = document.createElement('div');
+    parentNode.appendChild(div);
+}
+
+addDiv(body);
+addDiv(body);
+
+
+/*
+
+select body
+create outer flex div in that body
+create 4 column flex divs in that outer flex div
+create 4 divs in each column 
+
+*/
