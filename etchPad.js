3956fce2cf0ee60a18139feb050073f7e9659178
QSL <kbeechly@icloud.com>
2022-07-27 00:24:57 -0700

    Rearrange functions and rename add functions to create functions

--- etchPad.js
+++ etchPad.js
@@ -1,17 +1,25 @@
 function createEtchPadDisplay() {
     const body = document.querySelector('body');
-    addBox16(body);
+    createBox16(body);
     populateBox16();
 }
 
-function addBox16(parentNode) {
+function createBox16(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
     div.classList.add('box16');
     div.classList.add('flex');
 }
 
-function addColumnContainer(parentNode) {
+function populateBox16() {
+    const box16 = document.querySelector('.box16');
+    for (let i = 0; i<16; i++) {
+        createColumnContainer(box16);
+        populateColumnContainer();
+    }
+}
+
+function createColumnContainer(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
     div.classList.add('columnContainer');
@@ -19,27 +27,20 @@ function addColumnContainer(parentNode) {
     div.classList.add('column');
 }
 
-function addCellDiv(parentNode) {
+function populateColumnContainer() {
+    const columns = document.querySelectorAll('.columnContainer');
+    const column = columns[columns.length - 1];
+    for (let i = 0; i<16; i++) {
+        createCell(column);
+    }
+}
+
+function createCell(parentNode) {
     const div = document.createElement('div');
     parentNode.appendChild(div);
     div.classList.add('cell');
 }
 
-function populateColumnContainer() {
-    const columns = document.querySelectorAll('.columnContainer');
-    const column = columns[columns.length - 1];
-    for (let i = 0; i<16; i++) {
-        addCellDiv(column);
-    }
-}
-
-function populateBox16() {
-    const box16 = document.querySelector('.box16');
-    for (let i = 0; i<16; i++) {
-        addColumnContainer(box16);
-        populateColumnContainer();
-    }
-}
 
 // populateBox16();
 
